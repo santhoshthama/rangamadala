ec57a2f7e7169a97ce9d19549ebb1e0dd163c80d
RNWDissanayaka <rnwdissanayaka@gmail.com>
2026-04-12 11:16:32 +0530

    Remove migration for multi-booking decision from provider_availability table

--- dev/add_provider_date_bookings_table.sql
+++
@@ -1,2 +0,0 @@
--- Migration: Keep multi-booking decision simple in existing provider_availability table
-ALTER TABLE `provider_availability` ADD COLUMN IF NOT EXISTS `allow_more_bookings` tinyint(1) NOT NULL DEFAULT 1 COMMENT 'Summary: Can more bookings be added to this date?' AFTER `status`;
